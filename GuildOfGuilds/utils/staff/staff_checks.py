4cdaf97f4acd2c8da4a411e804c73495bf9322ce
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-08-16 19:18:51 -0400

    Create is_dev() and is_staff() decorators

--- GuildOfGuilds/utils/staff/staff_checks.py
+++ GuildOfGuilds/utils/staff/staff_checks.py
@@ -1,19 +1,40 @@
-def dev_check(users, author_id):
-    try:
-        if users.find_one({"id": author_id})["isDev"] is True:
-            print("true")
-            return True
-        else:
-            return False
-    except:
-        return False
+from discord.ext import commands
+from GuildOfGuilds.main import main_db
+users = main_db["users"]
+
+not_dev_error = "This command is for developers only!"
+not_staff_error = "This command is for staff members only!"
 
 
-def staff_check(users, author_id):
-    try:
-        if users.find_one({"id": author_id})["isStaff"] is True:
-            return True
+class StaffChecks(commands.CommandError):
+    pass
+
+
+def is_dev():
+    def predicate(ctx):
+        user = users.find_one({"id": ctx.author.id})
+        if user is None:
+            raise StaffChecks(not_dev_error)
+        elif "isDev" not in user:
+            raise StaffChecks(not_dev_error)
+        elif user["isDev"] is False:
+            raise StaffChecks(not_dev_error)
         else:
-            return False
-    except:
-        return False
+            return True
+
+    return commands.check(predicate)
+
+
+def is_staff():
+    def predicate(ctx):
+        user = users.find_one({"id": ctx.author.id})
+        if user is None:
+            raise StaffChecks(not_staff_error)
+        elif "isDev" not in user:
+            raise StaffChecks(not_staff_error)
+        elif user["isStaff"] is False:
+            raise StaffChecks(not_staff_error)
+        else:
+            return True
+
+    return commands.check(predicate)
