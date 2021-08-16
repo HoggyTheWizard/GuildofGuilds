1390a02b52813b2aeb206fbed7bb111721c31d0d
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-08-16 19:18:18 -0400

    Transfer link and unlink commands to new permission system

--- GuildOfGuilds/cogs/staff/link.py
+++ GuildOfGuilds/cogs/staff/link.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from GuildOfGuilds.utils.staff.staff_checks import dev_check
+from GuildOfGuilds.utils.staff.staff_checks import *
 from GuildOfGuilds.main import main_db
 import discord
 import requests
@@ -11,24 +11,25 @@ class link(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @is_staff()
     async def link(self, ctx, member: discord.Member, username):
-        if dev_check(users, ctx.author.id) is True:
-            try:
-                mojang = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{username}?').json()
-                if users.find_one({"id": member.id}) is None:
-                    users.insert_one({"id": member.id, "uuid": mojang["id"]})
-                    await ctx.send(f"Successfully linked {str(member)} to {mojang['name']} ({mojang['id']})")
-                else:
-                    await ctx.send("This user is already linked!")
-            except Exception as e:
-                print("Error")
-                await ctx.send(f"*Error**: {e}")
+        try:
+            mojang = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{username}?').json()
+            if users.find_one({"id": member.id}) is None:
+                users.insert_one({"id": member.id, "uuid": mojang["id"]})
+                await ctx.send(f"Successfully linked {str(member)} to {mojang['name']} ({mojang['id']})")
+            else:
+                await ctx.send("This user is already linked!")
+        except Exception as e:
+            print("Error")
+            await ctx.send(f"*Error**: {e}")
 
     @commands.command()
+    @is_staff()
     async def unlink(self, ctx, member: discord.Member):
-        if dev_check(users, ctx.author.id) is True:
             try:
-                users.delete_one({"id": ctx.author.id})
+                users.delete_one({"id": member.id})
+                await ctx.send(f"Successfully deleted data for {str(member)} ({member.id})")
             except Exception as e:
                 await ctx.send(f"*Error**: {e}")
 
