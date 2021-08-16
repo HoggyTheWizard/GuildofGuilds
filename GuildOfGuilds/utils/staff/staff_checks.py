c0e9c8e69aef242da6fac40162324810cd4c4186
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-08-16 10:56:28 -0400

    Revert "Create check_officer_list() and get_guild_member() functions"
    This reverts commit 5f24c37c1bb498aff4a63afd2c4255f9b2eea7bd.

--- GuildOfGuilds/utils/staff/staff_checks.py
+++ GuildOfGuilds/utils/staff/staff_checks.py
@@ -1,7 +1,6 @@
 def dev_check(users, author_id):
     try:
         if users.find_one({"id": author_id})["isDev"] is True:
-            print("true")
             return True
         else:
             return False
