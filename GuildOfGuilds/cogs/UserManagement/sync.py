97b3a2aead2246a3ee1fbb5594a1f938f77e07a8
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-08-17 08:42:24 -0400

    Finish development of user_sync task

--- GuildOfGuilds/cogs/UserManagement/sync.py
+++ GuildOfGuilds/cogs/UserManagement/sync.py
@@ -1,7 +1,9 @@
 from discord.ext import commands, tasks
 from GuildOfGuilds.config import hypixel_api_key, guild_id
+from GuildOfGuilds.utils.UserManagement.sync import *
 from GuildOfGuilds.main import main_db
 from GuildOfGuilds.utils.UserManagement.user import *
+from GuildOfGuilds.utils.UserManagement.guild import get_guild_member, check_officer_list
 import discord
 import asyncio
 import requests
@@ -14,7 +16,7 @@ class user_sync(commands.Cog):
         self.bot = bot
         # self.user_sync.start()
 
-    @tasks.loop(minutes=60)
+    @tasks.loop(minutes=30)
     async def user_sync(self):
         guild = self.bot.get_guild(guild_id)
         for user in users.find({}):
@@ -34,8 +36,40 @@ class user_sync(commands.Cog):
                 if member.name != data["username"]:
                     await member.edit(nick=format_nickname_from_db(data["tag"], data["username"]))
 
+            elif member.nick != f"{data['username']} [{data['tag']}]":
+                await member.edit(nick=format_nickname_from_db(data["tag"], data["username"]))
 
-    print("Done!")
+            if user["guildPosition"] is None:
+                continue
+            else:
+                guild_data = requests.get(
+                    f'https://api.hypixel.net/guild?key={hypixel_api_key}&player={mojang["id"]}').json()
+                gm = get_guild_member(guild_data, mojang["id"])
+                if gm["rank"] != user["guildPosition"]:
+                    if user["guildPosition"] == "officer":
+                        await member.remove_roles(guild.get_role(get_guild_role_from_db().get("guildmaster")))
+                    elif user["guildPosition"] == "guildmaster":
+                        await member.remove_roles(guild.get_role(get_guild_role_from_db().get("guildmaster")))
+
+                    if gm["rank"] == "GUILDMASTER":
+                        await member.add_roles(get_guild_role_from_db().get("guildmaster"), get_guild_role_from_db().get("officer"))
+                        users.update_one({"id": member.id}, {"$set": {"guildPosition": "guildmaster"}})
+
+                    elif check_officer_list(gm["rank"].lower()) is True:
+                        if user["guildPosition"] == "guildmaster":
+                            await member.remove_roles(get_guild_role_from_db().get("guildmaster"))
+
+                        await member.add_roles(get_guild_role_from_db().get("officer"))
+                        users.update_one({"id": member.id}, {"$set": {"guildPosition": "guildmaster"}})
+
+                    else:
+                        if user["guildPosition"] == "guildmaster":
+                            await member.add_roles(get_guild_role_from_db().get("guildmaster"),
+                                                   get_guild_role_from_db().get("officer"))
+                        await member.remove_roles(get_guild_role_from_db().get(user["guildPosition"]))
+                        users.update_one({"id": member.id}, {"$set": {"guildPosition": None}})
+        await asyncio.sleep(5)
+    print("Finished Sync Task")
 
     @user_sync.before_loop
     async def before_printer(self):
